345ac5a2c71336b16fe6aab205cbc8e573fd6f7b
joe <mrjoefgibson@gmail.com>
2022-10-25 14:39:27 -0500

    Adding box create folder again

---
+++ components/box/actions/create-folder-bu/create-folder-bu.mjs
@@ -0,0 +1,58 @@
+import app from "../../box.app.mjs";
+import axios from "axios";
+
+export default {
+	name       : "Create Folder BU",
+	version    : "0.0.2",
+	key        : "Create-folder-BU",
+	description: "Create a folder. [See the docs here](https://developer.box.com/reference/post-folders/)",
+	type       : "action",
+	props      : {
+		box           : {
+			type: "app",
+			app : "box",
+		},
+		parentFolderId: {
+			type       : "string",
+			label      : "New parent folder ID",
+			description: "The destination folder to copy the folder to.",
+		},
+		newFolderName : {
+			type       : "string",
+			label      : "New folder Name",
+			description: "An new name for the copied folder.",
+		}
+	},
+	methods    : {},
+	async run({steps, $}) {
+		var apiResult = await makeApiCall($, this);
+		var folderExists = apiResult.data.code === "item_name_in_use";
+		if(folderExists) {
+			return apiResult.data.context_info.conflicts;
+		} else {
+			return apiResult.data;
+		}
+	},
+};
+
+async function makeApiCall($, step) {
+	return await axios({
+		method        : 'post',
+		url           : `https://api.box.com/2.0/folders`,
+		headers       : {
+			"Authorization": `Bearer ${step.box.$auth.oauth_access_token}`,
+			"Content-Type" : "application/json",
+			"Accept"       : "application/json",
+			"User-Agent"   : "@PipedreamHQ/pipedream v0.1",
+		},
+		data          : {
+			parent: {
+				id: `${step.parentFolderId}`
+			},
+			name  : `${step.newFolderName}`
+		},
+		validateStatus: function(status) {
+			return status >= 200 || status == 409; // 409 means folder name already exists
+		},
+	})
+}
