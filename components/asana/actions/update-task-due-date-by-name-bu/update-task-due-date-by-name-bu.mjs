e58fd8bf715d55e9bc2feef3be7801eb3bc71d9b
joe <mrjoefgibson@gmail.com>
2022-10-25 16:43:51 -0500

    adding asana custom action

---
+++ components/asana/actions/update-task-due-date-by-name-bu/update-task-due-date-by-name-bu.mjs
@@ -0,0 +1,67 @@
+import asana from "../../asana.app.mjs";
+
+export default {
+	name       : "Update Task Due Date By Name BU",
+	version    : "0.0.1",
+	key        : "update-task-due-date-by-name-bu",
+	description: "Custom built to update tasks returned from get_all_project_tasks step",
+	props      : {
+      asana,
+      name                      : {
+          label      : "Name",
+          description: "Name of the task.",
+          type       : "string",
+      },
+      due_on                    : {
+          label      : "Due On",
+          description: "Due date from from submission.",
+          type       : "string",
+      },
+      get_all_project_tasks_data: {
+          label      : "Data from get_all_project_tasks step",
+          description: "Example: {{steps.get_all_project_tasks.$return_value.data}}",
+          type       : "string",
+      },
+	},
+	type       : "action",
+	methods    : {},
+	async run({$}) {
+		const taskGid = getTaskGid(this, $);
+        const formattedDueDate = formatDueDate(this.due_on);
+		const response = await this.asana._makeRequest(`tasks/${taskGid}`, {
+			method: "put",
+			data  : {
+				data: {
+					due_on: formattedDueDate,
+				},
+			},
+		}, $);
+
+		return response;
+	},
+};
+
+function formatDueDate(dueOn){
+  // given "Feb 01, 2023"
+  // output should be YYYY-MM-DD
+  var d = new Date(dueOn),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if(month.length < 2)
+    month = '0' + month;
+  if(day.length < 2)
+    day = '0' + day;
+
+  return [year,month,day].join('-');
+}
+
+function getTaskGid(step, $) {
+	for(let t = 0; t < step.get_all_project_tasks_data.length; t++) {
+		if(step.name === step.get_all_project_tasks_data[t].name) {
+			return step.get_all_project_tasks_data[t].gid;
+		}
+	}
+	return "";
+}
