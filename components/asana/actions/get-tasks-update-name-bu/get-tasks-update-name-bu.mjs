2edb13ecdfba6ace2adc1186877e642ca29a5e9f
joe <mrjoefgibson@gmail.com>
2022-11-02 16:39:25 -0500

    updating name replace

--- components/asana/actions/get-tasks-update-name-bu/get-tasks-update-name-bu.mjs
+++ components/asana/actions/get-tasks-update-name-bu/get-tasks-update-name-bu.mjs
@@ -2,7 +2,7 @@ import asana from "../../asana.app.mjs";
 
 export default {
 	name       : "Get Tasks Update Name BU",
-	version    : "0.0.4",
+	version    : "0.0.5",
 	key        : "get-tasks-update-name-bu",
 	description: "",
 	props      : {
@@ -92,7 +92,7 @@ async function updateTaskNames($, step, tasksToUpdate){
 	let updatedData = [];
 
 	for(let t = 0; t < tasksToUpdate.length; t++) {
-		let taskName = tasksToUpdate[t].name.replace(step.tour_year,"").trim();
+		let taskName = tasksToUpdate[t].name.replace("["+step.tour_year+"]","").replace(step.tour_year, "").trim();
 		let taskData = {};
 		let NewTaskName = step.tour_name + " " + step.tour_year + " - " + taskName;
 
