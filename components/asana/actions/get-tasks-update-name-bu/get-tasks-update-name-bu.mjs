5d4ecfb14752531ee2136d075a5a76a94f624d96
joe <mrjoefgibson@gmail.com>
2022-11-02 16:18:50 -0500

    updating task rename action

--- components/asana/actions/get-tasks-update-name-bu/get-tasks-update-name-bu.mjs
+++ components/asana/actions/get-tasks-update-name-bu/get-tasks-update-name-bu.mjs
@@ -2,7 +2,7 @@ import asana from "../../asana.app.mjs";
 
 export default {
 	name       : "Get Tasks Update Name BU",
-	version    : "0.0.3",
+	version    : "0.0.4",
 	key        : "get-tasks-update-name-bu",
 	description: "",
 	props      : {
@@ -22,6 +22,11 @@ export default {
 			description: "Add the path for the tour year variable",
 			type       : "string",
 		},
+		all_project_tasks_data:{
+			label      : "All Project Tasks Data",
+			description: "Use 'Get All Tasks In Project BU' step prior to this and add the return value. Example: {{steps.get_all_tasks_in_project_bu.$return_value}}",
+			type       : "string",
+		},
 		allTasks: {
 			type       : "string",
 			label      : "Do you want to add the tour name and year to all tasks?",
@@ -29,6 +34,7 @@ export default {
 				"Yes",
 				"No",
 			],
+			default: "Yes",
 			reloadProps: true,
 		},
 	},
@@ -36,20 +42,19 @@ export default {
 	methods    : {},
 	async additionalProps() {
 		let props = {};
+		let values;
 		if(this.allTasks === "No") {
-			const values = [
-				['task_list_to_update',
-				 "Task(s) to update name",
-				 "Comma separated list of task names to update with tour name."],
+			values = [
+				[
+					'task_list_to_update',
+					"Task(s) to update name",
+					"Comma separated list of task names to update with tour name."
+				],
 			];
-			if(!values.length) {
-				throw new ConfigurationError(
-					"Cound not find a header row. Please either add headers and click \"Refresh fields\" or adjust the action configuration to continue.");
-			}
 			for(let i = 0; i < values.length; i++) {
 				props[`${values[i][0]}`] = {
-					type    : "string",
-					label   : values[i][1],
+					type       : "string",
+					label      : values[i][1],
 					description: values[i][2]
 				};
 			}
@@ -57,38 +62,41 @@ export default {
 		return props;
 	},
 	async run({$}) {
-		let updatedData
+		let tasksToUpdate;
 		if(this.task_list_to_update.length){
-			updatedData = await updateIndividualTasksNames($, this);
+			tasksToUpdate = await getIndividualTasks($, this);
 		}else{
-			updatedData = await updateAllTaskNames($, this);
+			tasksToUpdate = this.all_project_tasks_data;
 		}
-		return updatedData;
+		return await updateTaskNames($, this, tasksToUpdate);
 	},
 };
 
-async function updateAllTaskNames($, step){
-	let allTasksArray = await getAllTasks($, step);
-
-	for(let t = 0; t < allTasksArray.data.length; t++) {
-
+async function getIndividualTasks($, step){
+	let taskListArray = step.task_list_to_update.split(',');
+	let allTasksArray = step.all_project_tasks_data;
+	let tasksToUpdate = [];
+	for(let i = 0; i < taskListArray.length; i++) {
+		let taskName = taskListArray[i].trim();
+		for(let t = 0; t < allTasksArray.length; t++) {
+			if(taskName === allTasksArray[t].name) {
+				tasksToUpdate.push(allTasksArray[t])
+				break;
+			}
+		}
 	}
+	return tasksToUpdate;
 }
 
-async function updateIndividualTasksNames($, step){
-	let taskListArray = step.task_list_to_update.split(',');
+async function updateTaskNames($, step, tasksToUpdate){
 	let updatedData = [];
 
-	for(let t = 0; t < taskListArray.length; t++) {
-		let taskName = taskListArray[t].trim();
+	for(let t = 0; t < tasksToUpdate.length; t++) {
+		let taskName = tasksToUpdate[t].name.replace(step.tour_year,"").trim();
 		let taskData = {};
-		let taskGid = await returnGidIfTaskCreated($, step, taskName);
-		if(taskGid === undefined) {
-			taskData.data = `${taskName} task not found after waiting 10+ seconds. ${taskName} due date not updated.`;
-		} else {
-			let NewTaskName = step.tour_name + " " + step.tour_year + " - " + taskListArray[t].trim();
-			taskData = await updateTaskName($, step, taskGid, NewTaskName);
-		}
+		let NewTaskName = step.tour_name + " " + step.tour_year + " - " + taskName;
+
+		taskData = await updateTaskName($, step, tasksToUpdate[t].gid, NewTaskName);
 		updatedData.push(taskData);
 	}
 	return updatedData;
@@ -104,40 +112,4 @@ async function updateTaskName($, step, taskGid, taskName) {
 		},
 	}, $);
 	return response;
-}
-
-async function checkIfAllTasksCreated($, step){
-
-}
-
-async function returnGidIfTaskCreated($, step, taskName) {
-	return await new Promise(resolve => {
-		let checkForTaskCreated = setInterval(async function() {
-			let trys = 1;
-			let allTasksArray = await getAllTasks($, step);
-			let taskGid = "";
-			for(let t = 0; t < allTasksArray.data.length; t++) {
-				if(taskName === allTasksArray.data[t].name) {
-					taskGid = allTasksArray.data[t].gid;
-					clearInterval(checkForTaskCreated);
-					resolve(taskGid);
-					break;
-				}
-			}
-			if(taskGid === "") {
-				if(trys >= 3) {
-					clearInterval(checkForTaskCreated);
-					resolve(taskGid);
-				}
-				trys++;
-			}
-		}, 3000);
-	})
-}
-
-async function getAllTasks($, step) {
-	return await step.asana._makeRequest(`projects/${step.project_gid}/tasks`, {
-		method: "get",
-		data  : {},
-	}, $);
 }
