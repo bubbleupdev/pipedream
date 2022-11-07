ecabf64c490d3636faa69926ab5a65e2086f0ac2
joe <mrjoefgibson@gmail.com>
2022-11-07 17:02:57 -0600

    adding gcf auth

---
+++ components/live_nation_custom/actions/call-gcf-with-auth/call-gcf-with-auth.mjs
@@ -0,0 +1,39 @@
+module.exports = defineComponent({
+	props: {
+		google_cloud: {
+			type: "app",
+			app : "google_cloud",
+		}
+	},
+	async run({steps, $}) {
+		const key = JSON.parse(this.google_cloud.$auth.key_json)
+		const {JWT} = require('google-auth-library');
+
+		async function main() {
+			const url = `https://us-central1-live-nation-358920.cloudfunctions.net/asana-add-custom-field-data-to-tasks`;
+			const client = new JWT({
+				email: key.client_email,
+				key  : key.private_key,
+			});
+			const token = await client.fetchIdToken(url);
+			const headers = new Map([
+				[
+					'Authorization',
+					`Bearer ${token}`
+				],
+				[
+					'Content-Type',
+					`application/json`
+				]
+			]);
+			const payload = {"task_ids": ["1203247329009629"], "custom_fields": {"1203162503620378": "marketing brief link"}}
+			const res = await client.request({
+				url, headers, method: 'POST',
+				data                : JSON.stringify(payload)
+			});
+			console.log(res.data);
+		}
+
+		return await main()
+	},
+})
