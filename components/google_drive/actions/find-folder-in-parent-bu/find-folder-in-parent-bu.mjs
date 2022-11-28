b849d89e76cb14afb989bd1e409c632a3b861d48
joegibson <mrjoefgibson@gmail.com>
2022-11-28 17:30:12 -0600

    returning found folders

--- components/google_drive/actions/find-folder-in-parent-bu/find-folder-in-parent-bu.mjs
+++ components/google_drive/actions/find-folder-in-parent-bu/find-folder-in-parent-bu.mjs
@@ -9,7 +9,7 @@ export default {
 	key        : "google_drive-find-folder",
 	name       : "Find Folder BU",
 	description: "Find a folder. [See the docs](https://developers.google.com/drive/api/v3/reference/files/create) for more information",
-	version    : "0.0.6",
+	version    : "0.0.7",
 	type       : "action",
 	props      : {
 		googleDrive,
@@ -47,6 +47,7 @@ async function findChildWithinParent(parentId, childName, drive, googleDrive)
 	console.log(`Looking for ${childName} within ${parentId}`);
 	const query = `"${parentId}" in parents and trashed=false and mimeType = "application/vnd.google-apps.folder" and name = "${childName}"`;
 	const getlist = getListFiles(drive, {q: query,});
+	console.log(`getlist: ${getlist}`)
 	safetyBug(getlist)
 	let folders = (await googleDrive.listFilesInPage(null, getlist)).files;
 	return folders;
