4780c3285711ed39fb5b5b50ccbe35bbb3cb990d
joegibson <mrjoefgibson@gmail.com>
2022-11-28 14:17:11 -0600

    adding logging

--- components/google_drive/actions/create-folder-bu/create-folder-bu.mjs
+++ components/google_drive/actions/create-folder-bu/create-folder-bu.mjs
@@ -9,7 +9,7 @@ export default {
   key: "google_drive-create-folder",
   name: "Create Folder BU",
   description: "Create a new empty folder. [See the docs](https://developers.google.com/drive/api/v3/reference/files/create) for more information",
-  version: "0.0.22",
+  version: "0.0.23",
   type: "action",
   props: {
     googleDrive,
