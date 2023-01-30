fd20a085992a479730ce0c3709f620341e049a48
joegibson <mrjoefgibson@gmail.com>
2023-01-30 15:47:56 -0600

    adding new stuff back

--- components/live_nation_custom/actions/asana-update-task-names-to-gcf-bu/asana-update-task-names-to-gcf.mjs
+++ components/live_nation_custom/actions/asana-update-task-names-to-gcf-bu/asana-update-task-names-to-gcf.mjs
@@ -1,6 +1,6 @@
 module.exports = defineComponent({
 	name   : "Asana Update Task Names To GCF",
-	version: "0.0.6",
+	version: "0.0.7",
 	key    : "asana-update-task-names-to-gcf-bu",
 	props: {
 		google_cloud: {
@@ -15,6 +15,36 @@ module.exports = defineComponent({
 			label: "Tour Name and Year",
 			type : "string",
 		},
+		name                  : {
+			label      : "Artist Name",
+			description: "Headliner",
+			type       : "string",
+		},
+		co_headliner_1        : {
+			label      : "Co-Headliner 1",
+			description: "1st Co-Headliner",
+			type       : "string",
+		},
+		co_headliner_2        : {
+			label      : "Co-Headliner 2",
+			description: "2nd Co-Headliner",
+			type       : "string",
+		},
+		co_headliner_3        : {
+			label      : "Co-Headliner 3",
+			description: "3rd Co-Headliner",
+			type       : "string",
+		},
+		co_headliner_4        : {
+			label      : "Co-Headliner 4",
+			description: "4th Co-Headliner",
+			type       : "string",
+		},
+		year                  : {
+			label      : "Year",
+			description: "Year of the project.",
+			type       : "string",
+		},
 		all_project_tasks_data: {
 			label      : "All Project Tasks Data",
 			description: "Use 'Get All Tasks In Project BU' step prior to this and add the return value. Example: {{steps.get_all_tasks_in_project_bu.$return_value}}",
@@ -24,6 +54,18 @@ module.exports = defineComponent({
 	type   : "action",
 	methods: {},
 	async run({steps, $}) {
+		let projectName = this.name;
+		let coHeadliners = [
+			this.co_headliner_1,
+			this.co_headliner_2,
+			this.co_headliner_3,
+			this.co_headliner_4,
+		]
+		for(let i = 0; i < coHeadliners.length; i++) {
+			projectName = concatName(projectName, coHeadliners[i]);
+		}
+		projectName = projectName + ' ' + this.year;
+
 		const key = JSON.parse(this.google_cloud.$auth.key_json)
 		const {JWT} = require('google-auth-library');
 
@@ -39,7 +81,7 @@ module.exports = defineComponent({
 		    ['Authorization', `Bearer ${token}`],
 		    ['Content-Type', `application/json`]
 		  ]);
-		  let payload = {"tour_name_year": step.tour_name_year};
+		  let payload = {"tour_name_year": projectName};
 		  payload.all_project_tasks_data = step.all_project_tasks_data;
 		  console.log(payload);
 		  client.request({url,headers,method: 'POST',data: payload});
@@ -48,3 +90,10 @@ module.exports = defineComponent({
 		return await main(this);
 	},
 })
+
+function concatName(projectName, coHeadliner) {
+	if(coHeadliner) {
+		return projectName + ' / ' + coHeadliner;
+	}
+	return projectName;
+}
