7568bb746fbebe7b990abfbfdf99d87a05a307bb
joegibson <mrjoefgibson@gmail.com>
2023-01-31 15:26:03 -0600

    updating to use coheadliners and removing actual update request for testing purposes

--- components/google_sheets/actions/update-cell-bu/update-cell-bu.mjs
+++ components/google_sheets/actions/update-cell-bu/update-cell-bu.mjs
@@ -10,7 +10,7 @@ export default {
   key        : "update-cell-bu",
   name       : "Update Cell BU",
   description: "Update a cell in a spreadsheet",
-  version    : "0.0.8",
+  version    : "0.0.9",
   type       : "action",
   props      : {
     googleSheets,
@@ -37,11 +37,36 @@ export default {
       type    : "string",
       label   : "Sheet values from prior step",
     },
-    eventName: {
-      type    : "string",
-      label   : spreadsheetProps[0],
-      optional: true,
-    },
+	  name          : {
+		  label      : "Artist Name",
+		  description: "Headliner",
+		  type       : "string",
+	  },
+	  co_headliner_1: {
+		  label      : "Co-Headliner 1",
+		  description: "1st Co-Headliner",
+		  type       : "string",
+	  },
+	  co_headliner_2: {
+		  label      : "Co-Headliner 2",
+		  description: "2nd Co-Headliner",
+		  type       : "string",
+	  },
+	  co_headliner_3: {
+		  label      : "Co-Headliner 3",
+		  description: "3rd Co-Headliner",
+		  type       : "string",
+	  },
+	  co_headliner_4: {
+		  label      : "Co-Headliner 4",
+		  description: "4th Co-Headliner",
+		  type       : "string",
+	  },
+	  year          : {
+		  label      : "Year",
+		  description: "Year of the project.",
+		  type       : "string",
+	  },
     tourMarketer     : {
       type    : "string",
       label   : spreadsheetProps[1],
@@ -54,69 +79,39 @@ export default {
     },
   },
   async run({$}) {
+	  let projectName = this.name;
+	  let coHeadliners = [
+		  this.co_headliner_1,
+		  this.co_headliner_2,
+		  this.co_headliner_3,
+		  this.co_headliner_4,
+	  ]
+	  for(let i = 0; i < coHeadliners.length; i++) {
+		  projectName = concatName(projectName, coHeadliners[i]);
+	  }
+	  projectName = projectName + ' ' + this.year;
+
     var spreadsheetPropValues = [
-      this.eventName,
+		projectName,
       this.tourMarketer,
       this.romeTourMarketingID
     ]
     let sheetValues = this.sheetValues;
-    var values = [];
-    var columnConversion = [
-        "A",
-        "B",
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "H"
-    ];
 
-    function exists(arr, search) {
-      return arr.some(row => row.includes(search));
-    }
-
-    function multidimentionalForSearchLoop(propItem) {
-      for(let i = 0; i < sheetValues?.length; i++) {
-        for(let j = 0; j < sheetValues[i]?.length; j++) {
-          if(sheetValues[i][j] === propItem) {
-            let row = i + 1;
-            let columnForNewValue;
-            // DEPENDANT IF
-            if(propItem === spreadsheetProps[0]){
-              console.log("sheetValues[i][j] : '" + sheetValues[i][j] + "'");
-              // Replacing same cell
-              columnForNewValue = columnConversion[0];
-            }else{
-              console.log("sheetValues[i][j] : '" + sheetValues[i][j] + "'");
-              console.log("columnConversion[j + 1] : '" + columnConversion[j + 1] + "'");
-              // Moving one column to the right
-              columnForNewValue = columnConversion[j + 1];
-            }
-            let cellForNewValue = columnForNewValue + row;
-            console.log("propItem : '" + propItem + "'");
-            console.log("cellForNewValue : '" + cellForNewValue + "'");
-            values.push([
-              propItem,
-              cellForNewValue
-            ]);
-          }
-        }
-      }
-    }
-
+	let sheetValueReplacements = [];
     for(let i = 0; i < spreadsheetProps?.length; i++) {
       if(exists(sheetValues, spreadsheetProps[i])) {
-        multidimentionalForSearchLoop(spreadsheetProps[i]);
+	      sheetValueReplacements.push(multidimentionalForSearchLoop(spreadsheetProps[i]));
       }
     }
+
     var updatingValueAndCells = [];
-    $.export("$summary", values);
-    for(let i = 0; i < values?.length; i++) {
-      updatingValueAndCells.push({value: spreadsheetPropValues[i], cell: values[i][1]});
+    $.export("$summary", sheetValueReplacements);
+    for(let i = 0; i < sheetValueReplacements?.length; i++) {
+      updatingValueAndCells.push({value: spreadsheetPropValues[i], cell: sheetValueReplacements[i][1]});
       const request = {
         spreadsheetId   : this.sheetId,
-        range           : `${this.sheetName}!${values[i][1]}:${values[i][1]}`,
+        range           : `${this.sheetName}!${sheetValueReplacements[i][1]}:${sheetValueReplacements[i][1]}`,
         valueInputOption: "USER_ENTERED",
         resource        : {
           values: [
@@ -126,8 +121,51 @@ export default {
           ],
         },
       };
-      await this.googleSheets.updateSpreadsheet(request);
+      //await this.googleSheets.updateSpreadsheet(request);
     }
     return "Updated: " + JSON.stringify(updatingValueAndCells);
   },
 };
+
+function concatName(projectName, coHeadliner) {
+	if(coHeadliner) {
+		return projectName + ' / ' + coHeadliner;
+	}
+	return projectName;
+}
+
+function exists(arr, search) {
+	return arr.some(row => row.includes(search));
+}
+
+function multidimentionalForSearchLoop(propItem, sheetValues, spreadsheetProps) {
+	let columnConversion = ["A", "B", "C", "D", "E", "F", "G", "H"];
+	let values = [];
+	for(let i = 0; i < sheetValues?.length; i++) {
+		for(let j = 0; j < sheetValues[i]?.length; j++) {
+			if(sheetValues[i][j] === propItem) {
+				let row = i + 1;
+				let columnForNewValue;
+				// DEPENDANT IF
+				if(propItem === spreadsheetProps[0]) {
+					console.log("sheetValues[i][j] : '" + sheetValues[i][j] + "'");
+					// Replacing same cell
+					columnForNewValue = columnConversion[0];
+				} else {
+					console.log("sheetValues[i][j] : '" + sheetValues[i][j] + "'");
+					console.log("columnConversion[j + 1] : '" + columnConversion[j + 1] + "'");
+					// Moving one column to the right
+					columnForNewValue = columnConversion[j + 1];
+				}
+				let cellForNewValue = columnForNewValue + row;
+				console.log("propItem : '" + propItem + "'");
+				console.log("cellForNewValue : '" + cellForNewValue + "'");
+				values.push([
+					propItem,
+					cellForNewValue
+				]);
+			}
+		}
+	}
+	return values;
+}
