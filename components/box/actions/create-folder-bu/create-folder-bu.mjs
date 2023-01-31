3586d910a7c16c080ef2f76e540987898e262350
joegibson <mrjoefgibson@gmail.com>
2023-01-31 10:55:48 -0600

    updating to use coheadliners

--- components/box/actions/create-folder-bu/create-folder-bu.mjs
+++ components/box/actions/create-folder-bu/create-folder-bu.mjs
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default {
 	name       : "Create Folder BU",
-	version    : "0.0.3",
+	version    : "0.0.4",
 	key        : "Create-folder-BU",
 	description: "Create a folder. [See the docs here](https://developer.box.com/reference/post-folders/)",
 	type       : "action",
@@ -21,10 +21,46 @@ export default {
 			type       : "string",
 			label      : "New folder Name",
 			description: "An new name for the copied folder.",
-		}
+		},
+		name          : {
+			label      : "Artist Name",
+			description: "Headliner",
+			type       : "string",
+		},
+		co_headliner_1: {
+			label      : "Co-Headliner 1",
+			description: "1st Co-Headliner",
+			type       : "string",
+		},
+		co_headliner_2: {
+			label      : "Co-Headliner 2",
+			description: "2nd Co-Headliner",
+			type       : "string",
+		},
+		co_headliner_3: {
+			label      : "Co-Headliner 3",
+			description: "3rd Co-Headliner",
+			type       : "string",
+		},
+		co_headliner_4: {
+			label      : "Co-Headliner 4",
+			description: "4th Co-Headliner",
+			type       : "string",
+		},
 	},
 	methods    : {},
 	async run({steps, $}) {
+		let projectName = this.name;
+		let coHeadliners = [
+			this.co_headliner_1,
+			this.co_headliner_2,
+			this.co_headliner_3,
+			this.co_headliner_4,
+		]
+		for(let i = 0; i < coHeadliners.length; i++) {
+			projectName = concatName(projectName, coHeadliners[i]);
+		}
+
 		var apiResult = await makeApiCall($, this);
 		var folderExists = apiResult.data.code === "item_name_in_use";
 		if(folderExists) {
@@ -34,6 +70,13 @@ export default {
 		}
 	},
 };
+
+function concatName(projectName, coHeadliner) {
+	if(coHeadliner) {
+		return projectName + ' / ' + coHeadliner;
+	}
+	return projectName;
+}
 
 async function makeApiCall($, step) {
 	return await axios({
