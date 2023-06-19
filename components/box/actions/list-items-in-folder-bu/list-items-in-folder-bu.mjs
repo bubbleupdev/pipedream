bf6af390006d645a3c0c7db220b09d44ee14e86e
joegibson <mrjoefgibson@gmail.com>
2023-06-19 13:48:21 -0500

    adding new action

---
+++ components/box/actions/list-items-in-folder-bu/list-items-in-folder-bu.mjs
@@ -0,0 +1,39 @@
+import app from "../../box.app.mjs";
+import axios from "axios";
+
+export default {
+	name       : "List items in folder (BU)",
+	version    : "0.0.1",
+	key        : "list-items-in-folder-bu",
+	description: "List items in a given folder",
+	type       : "action",
+	props      : {
+		box           : {
+			type: "app",
+			app : "box",
+		},
+		folderId: {
+			type       : "string",
+			label      : "Folder ID",
+			description: "Box folder ID",
+		}
+	},
+	methods    : {},
+	async run({steps, $}) {
+		var apiResult = await makeApiCall($, this);
+		return apiResult.data;
+	},
+};
+
+async function makeApiCall($, step) {
+	return await axios({
+		method        : 'post',
+		url           : `https://api.box.com/2.0/folders/${step.folderId}/items`,
+		headers       : {
+			"Authorization": `Bearer ${step.box.$auth.oauth_access_token}`,
+			"Content-Type" : "application/json",
+			"Accept"       : "application/json",
+			"User-Agent"   : "@PipedreamHQ/pipedream v0.1",
+		},
+	});
+}
